1775ad76ae74db296126f467b0bdf0f775225c35
Vincenzo Pascarella <vincenz.pascarella@gmail.com>
2023-05-18 12:02:21 +0200

    Initial Commit

---
+++ LottieAnimation/LottieAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationApp.swift
+//  LottieAnimation
+//
+//  Created by Vincenzo Pascarella on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
